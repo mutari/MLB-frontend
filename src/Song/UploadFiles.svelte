c881c05cf9ffe0eba5ffc3af88d4754c7fb37434
Philip johansson <pjohansson01@yahoo.com>
2020-12-03 15:08:34 +0100

    init

--- src/Song/UploadFiles.svelte
+++ src/Song/UploadFiles.svelte
@@ -1,53 +1,63 @@
 <script>
 
-function handleSubmit(e) {
-    e.preventDefault();
-    console.log(e)
-}
+import { send } from '../Tools/crud.js';
 
+async function handleSubmit(e) {
+
+    let title = document.querySelector('input[type="text"]');
+    let file = document.querySelector('input[type="file"]');
+    let textarea = document.querySelector('textArea');
+
+    e.preventDefault();
+
+    var headers = new Headers();
+    headers.set('x-api-key', 'awjfiohaiguhseuyg7w7yuhseucuwge7fw78e')
+
+    var formData = new FormData();
+    formData.append('title', title.value)
+    formData.append('textLyrics', textarea.value)
+    formData.append('fileLyrics', file.files[0])
+
+    let response = await send('http://mylyricbook/api/addLyric', {
+        method: 'POST',
+        headers,
+        body: formData
+    })
+
+    console.log(response);
+
+}
+/*
 function onFileUploadeChange(e) {
     console.log(e.target.files)
     let files = e.target.files;
 
     let div = document.querySelector('#fileArray');
-    for(var i = 0; i < files.length; i++) {
-        console.log(files[i]);
-        let newDiv = document.createElement('div');
-        let input = document.createElement('input');
-        input.setAttribute('type', 'text')
-        input.setAttribute('name', 'title')
-        input.setAttribute('placeholder', 'title')
-        newDiv.append(input);
-        input = document.createElement('textArea');
-        input.setAttribute('name', 'text')
-        newDiv.append(input);
-        div.append(newDiv)
-        input = document.createElement('input');
-        input.setAttribute('type', 'file')
-        input.setAttribute('name', 'file')
-        newDiv.append(input);
 
-        /**
-         * document.createElement('div').innerHTML = `<input type="text" name="title" placeholder="title">
-            <input type="file" name="file">
-            <textArea name="text"></textArea>`
-        */
+    let latest = document.querySelector(`#number-${numberOfUploades}`);
+    if(latest.querySelector('input[type="text"]').value == "" && latest.querySelector('input[type="file"]').files.length == 0 && latest.querySelector('textArea').value == "") {
+        numberOfUploades--;
+        latest.remove();
+    }
+
+    for(var i = 0; i < files.length; i++) {
+        numberOfUploades++;
+        div.append(addNewUploadeField(files[i]));
     }
 
 }
-
+*/
 </script>
 
 <form on:submit={handleSubmit}>
-
+<!--
     <input type="file" name="files" on:change={onFileUploadeChange} multiple>
-
+-->
     <div id="fileArray">
-        <div>
-            <input type="text" name="title" placeholder="title">
-            <textArea name="text"></textArea>
-            <input type="file" name="file">
-        </div>
+        <input type="text" name="title" placeholder="title">
+        <textArea name="text"></textArea>
+        <input type="file" name="filelyrics">
+        <input type="submit" value="upload">
     </div>
 
 </form>
--- src/Tools/crud.js
+++ src/Tools/crud.js
@@ -1,6 +1,11 @@
+async function send(url, setting) {
+    const response = await fetch(url, setting);
+    return await response.json();
+}
+
 async function getAllTitles() {
     const response = await fetch('http://mylyricbook/api/')    
     return await response.json();
 }
 
-export { getAllTitles }
+export { getAllTitles, send }
